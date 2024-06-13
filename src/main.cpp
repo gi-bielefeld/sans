6f74fa0697f29c6de8968277c4ff775643100922
Fabian <fabian.kolesch@uni-bielefeld.de>
2024-06-13 17:05:54 +0200

    [BUILD] Added experimental output sync

--- src/main.cpp
+++ src/main.cpp
@@ -1004,6 +1004,7 @@ int main(int argc, char* argv[]) {
         // Thread safe implementation of getting the index of the next input to preocess
         uint64_t index = 0;
         std::mutex index_mutex;
+        std::mutex sync_mutex;
         auto index_lambda = [&] () { std::lock_guard<mutex> lg(index_mutex); return index++;};
 
         auto lambda = [&] (uint64_t T, vector<uint16_t> genome_ids, vector<uint16_t> file_ids){ // This lambda expression wraps the sequence-kmer hashing
@@ -1024,7 +1025,8 @@ int main(int argc, char* argv[]) {
 					if (q_table.size()>0) {
 						cout <<" q="<<q_table[genome_ids[i]];
 					}
-					cout << " (genome " << genome_ids[i]+1 << "/" << denom_file_count;
+                    $SYNC( sync_mutex, $log $_ << " (genome " << genome_ids[i]+1 << "/" << denom_file_count << _$ );
+					// cout << " (genome " << genome_ids[i]+1 << "/" << denom_file_count;
 					if(genome_ids.size()>gen_files.size()){
 						cout << "; file " << i+1 << "/" << genome_ids.size();
 					}
--- src/util.h
+++ src/util.h
@@ -10,9 +10,35 @@
 #include <vector>
 #include <regex>
 #include <sys/stat.h>
-
+// #include <mutex>
 
 using namespace std;
+
+
+/** 
+Makro definitions for advanced output options
+(Transcribed from kc)
+*/
+#define $out  std::cout
+#define $log  std::cerr
+
+#define $err  std::cerr << "\33[38;5;131m"
+#define $warn std::cerr << "\33[38;5;137m"
+#define $note std::cerr << "\33[38;5;132m"
+
+#define $link std::cerr << "\33[38;5;67m"
+#define $lite std::cerr << "\33[38;5;244m"
+#define $_              << "\33[2K\r"
+
+#define  $                 std::flush
+#define  end$              std::endl
+#define  end$$             std::endl, exit(0)
+
+#define _$     "\33[0m" << std::flush
+#define _end$  "\33[0m" << std::endl
+#define _end$$ "\33[0m" << std::endl, exit(1)
+#define $SYNC(mtx, STREAM) mtx.lock(), STREAM, mtx.unlock()
+
 
 /**
  * This class contains some helpful utility functions.
